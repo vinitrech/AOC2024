f2cb1a0ad60e9f87deca7a0ea69a024a6c24df20
Vinicius Rech <vinitrech@gmail.com>
2024-12-09 11:35:40 -0300

    finishing day 2 second half

--- AOC2024/DayOne/Program.cs
+++ AOC2024/DayOne/Program.cs
@@ -1,45 +1,44 @@
-﻿namespace DayOne
+﻿namespace DayOne;
+
+public class Program
 {
-    public class Program
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
-        {
-            var timeToRun = TimeProvider.System.GetTimestamp();
-            var (leftList, rightList) = await ParseInputListsAsync();
-            int totalSum = SumDifferences(leftList, rightList);
-            int similarityScore = CalculateSimilarity(leftList, rightList);
+        var timeToRun = TimeProvider.System.GetTimestamp();
+        var (leftList, rightList) = await ParseInputListsAsync();
+        int totalSum = SumDifferences(leftList, rightList);
+        int similarityScore = CalculateSimilarity(leftList, rightList);
 
-            Console.WriteLine($"Sum: {totalSum}");
-            Console.WriteLine($"Similarity Score: {similarityScore}");
-            Console.WriteLine($"Time to run: {TimeProvider.System.GetElapsedTime(timeToRun).TotalMilliseconds}");
+        Console.WriteLine($"Sum: {totalSum}");
+        Console.WriteLine($"Similarity Score: {similarityScore}");
+        Console.WriteLine($"Time to run: {TimeProvider.System.GetElapsedTime(timeToRun).TotalMilliseconds}");
+    }
+
+    private static async Task<(List<int> LeftList, List<int> RightList)> ParseInputListsAsync()
+    {
+        var inputLines = await File.ReadAllLinesAsync("input.txt");
+        var (leftList, rightList) = (new List<int>(inputLines.Length), new List<int>(inputLines.Length));
+
+        for (var index = 0; index < inputLines.Length; index++)
+        {
+            var numbers = inputLines[index].Split(',');
+
+            leftList.Add(Convert.ToInt32(numbers[0]));
+            rightList.Add(Convert.ToInt32(numbers[1]));
         }
 
-        private static async Task<(List<int> LeftList, List<int> RightList)> ParseInputListsAsync()
-        {
-            var inputLines = await File.ReadAllLinesAsync("input.txt");
-            var (leftList, rightList) = (new List<int>(inputLines.Length), new List<int>(inputLines.Length));
+        return (leftList, rightList);
+    }
 
-            for (var index = 0; index < inputLines.Length; index++)
-            {
-                var numbers = inputLines[index].Split(',');
+    private static int SumDifferences(List<int> leftList, List<int> rightList) =>
+        leftList.Order()
+            .Zip(rightList.Order(), (f, s) => Math.Abs(f - s))
+            .Sum();
 
-                leftList.Add(Convert.ToInt32(numbers[0]));
-                rightList.Add(Convert.ToInt32(numbers[1]));
-            }
+    private static int CalculateSimilarity(List<int> leftList, List<int> rightList)
+    {
+        var hashSet = new HashSet<int>(leftList);
 
-            return (leftList, rightList);
-        }
-
-        private static int SumDifferences(List<int> leftList, List<int> rightList) =>
-            leftList.Order()
-                .Zip(rightList.Order(), (f, s) => Math.Abs(f - s))
-                .Sum();
-
-        private static int CalculateSimilarity(List<int> leftList, List<int> rightList)
-        {
-            var hashSet = new HashSet<int>(leftList);
-
-            return rightList.Where(hashSet.Contains).Sum();
-        }
+        return rightList.Where(hashSet.Contains).Sum();
     }
 }
--- AOC2024/DayTwo/Program.cs
+++ AOC2024/DayTwo/Program.cs
@@ -1,31 +1,49 @@
-﻿namespace DayTwo
+﻿namespace DayTwo;
+
+internal class Program
 {
-    internal class Program
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
-        {
-            var timeToRun = TimeProvider.System.GetTimestamp();
-            var reports = await ParseInputListAsync();
-            var validReportsCount = reports.Count(IsValidReport);
+        var timeToRun = TimeProvider.System.GetTimestamp();
+        var reports = await ParseInputListAsync();
+        var validReportsCount = reports.Count(IsValidReport);
 
-            Console.WriteLine($"Valid reports: {validReportsCount}");
-            Console.WriteLine($"Time to run: {TimeProvider.System.GetElapsedTime(timeToRun).TotalMilliseconds}");
-        }
+        Console.WriteLine($"Valid reports: {validReportsCount}");
+        Console.WriteLine($"Time to run: {TimeProvider.System.GetElapsedTime(timeToRun).TotalMilliseconds}");
+    }
 
-        private static bool IsValidReport(IEnumerable<int> elements) =>
-            ValidateReport(elements, ascending: true) || ValidateReport(elements, ascending: false);
+    private static bool IsValidReport(IEnumerable<int> elements) =>
+        ValidateReport(elements, ascending: true) || ValidateReport(elements, ascending: false);
 
-        private static bool ValidateReport(IEnumerable<int> elements, bool ascending) =>
-            elements.Zip(elements.Skip(1), (f, s) => IsValidPair(f, s, ascending)).All(x => x);
+    private static bool ValidateReport(IEnumerable<int> elements, bool ascending) =>
+        IsValidSequence(elements, ascending) || HasValidPermutation(elements, ascending);
 
-        private static bool IsValidPair(int f, int s, bool ascending) =>
-            (ascending ? f < s : f > s) && Math.Abs(f - s) >= 1 && Math.Abs(f - s) <= 3;
+    private static bool HasValidPermutation(IEnumerable<int> elements, bool ascending)
+    {
+        var validityList = GetElementsValidity(elements, ascending).ToList();
+        var faultyIndex = validityList.FindIndex(e => !e);
+        var leftPermutation = ExcludeAt(elements, faultyIndex);
+        var rightPermutation = ExcludeAt(elements, faultyIndex + 1);
 
-        private static async Task<IEnumerable<IEnumerable<int>>> ParseInputListAsync()
-        {
-            var inputLines = await File.ReadAllLinesAsync("input.txt");
+        return IsValidSequence(leftPermutation, ascending) || IsValidSequence(rightPermutation, ascending);
+    }
 
-            return inputLines.Select(l => l.Split(' ').Select(i => Convert.ToInt32(i)));
-        }
+    private static IEnumerable<int> ExcludeAt(IEnumerable<int> elements, int index) =>
+        elements.Where((_, i) => i != index);
+
+    private static bool IsValidSequence(IEnumerable<int> sequence, bool ascending) =>
+        GetElementsValidity(sequence, ascending).All(x => x);
+
+    private static IEnumerable<bool> GetElementsValidity(IEnumerable<int> elements, bool ascending) =>
+        elements.Zip(elements.Skip(1), (f, s) => IsValidPair(f, s, ascending));
+
+    private static bool IsValidPair(int f, int s, bool ascending) =>
+        (ascending ? f < s : f > s) && Math.Abs(f - s) >= 1 && Math.Abs(f - s) <= 3;
+
+    private static async Task<IEnumerable<IEnumerable<int>>> ParseInputListAsync()
+    {
+        var inputLines = await File.ReadAllLinesAsync("input.txt");
+
+        return inputLines.Select(l => l.Split(' ').Select(i => Convert.ToInt32(i)));
     }
 }
