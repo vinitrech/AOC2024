f312a8b732a4d7eaf218e8512d72cfa0655c6d5d
Vinicius Rech <vinitrech@gmail.com>
2024-12-09 11:56:49 -0300

    solving day 3 - half 1

---
+++ AOC2024/DayThree/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DayThree;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var timeToRun = TimeProvider.System.GetTimestamp();
+        var matchesList = await ParseInputListAsync();
+        var sum = matchesList.SelectMany(SelectProducts).Sum();
+
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Time to run: {TimeProvider.System.GetElapsedTime(timeToRun).TotalMilliseconds}");
+    }
+
+    private static IEnumerable<int> SelectProducts(MatchCollection matches) =>
+        matches.Select(m => Convert.ToInt32(m.Groups[1].Value) * Convert.ToInt32(m.Groups[2].Value));
+
+    private static async Task<IEnumerable<MatchCollection>> ParseInputListAsync()
+    {
+        var inputLines = await File.ReadAllLinesAsync("input.txt");
+
+        return inputLines.Select(l => Regex.Matches(l, @"mul\((\d{1,3}),(\d{1,3})\)", RegexOptions.Compiled, TimeSpan.FromMilliseconds(1000)));
+    }
+}
